6749484c81c031f7f94d4e8b54431e6045ce01b4
Cara Kane <cara.kane@gmail.com>
2017-10-04 11:30:59 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
 import {WrapperApp} from './App'
@@ -7,7 +6,6 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
-// import rootReducer from './reducers/index'
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
